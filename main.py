Fixed stack overflow from recursive event handling

main.py
@@ -5,6 +5,7 @@ import numpy as np
 import mathutils
 import math
 import shutil
+from bpy.app.handlers import persistent
 
 # Make python able to import from scripts in current working directory # TODO: fix this with __init__
 dir = os.getcwd()
@@ -12,17 +13,36 @@ if not dir in sys.path:
     sys.path.append(dir)
 from utils import *
 
-idx = 0
+bpy.context.preferences.addons["cycles"].preferences.get_devices()
 
+# Set blender settings
+bpy.context.preferences.addons["cycles"].preferences.compute_device_type = "CUDA"
+bpy.context.scene.cycles.device = "GPU"
+
+# get_devices() to let Blender detects GPU device
+
+
+print('Cycles compute device data (should be CUDA)')
+print(bpy.context.preferences.addons["cycles"].preferences.compute_device_type)
+
+print('All devices:')
+for d in bpy.context.preferences.addons["cycles"].preferences.devices:
+    print(d["name"], str(d["use"]))
+
+
+# Set blender scene parameters
 bpy.context.scene.render.use_compositing = True
 bpy.context.scene.use_nodes = True
 scene = bpy.context.scene
+scene.cycles.device = "GPU"
 render = scene.render
 
 # Output directory
 OUTPUT_DIR = os.path.join(os.getcwd(), 'output')
-GT_IMAGE = 'output/Image0000.png'
-BACKGROUND_DIR = 'C:\data\indoorCVPR_09\Images\meeting_room'
+GT_IMAGE = os.path.join(os.getcwd(), 'output', 'Image0000.png')
+BACKGROUND_DIR = os.path.join(os.pardir, 'indoorCVPR_09', 'Images', 'meeting_room')
+#GT_IMAGE = 'output/Image0000.png'
+#BACKGROUND_DIR = 'C:\data\indoorCVPR_09\Images\meeting_room'
 
 # Scene objects
 cam = bpy.data.objects['Camera']
@@ -34,7 +54,7 @@ print(scene_lights)
 background_images = [os.path.join(BACKGROUND_DIR, file) for file in os.listdir(BACKGROUND_DIR)]
 
 
-def render_scene_handler(*args, **kwargs):
+def randomize_scene_handler(*args, **kwargs):
     """
     Handler that runs on render completion to start new render. The scene can
     be randomized here for different objects location, lighting, etc.
@@ -43,10 +63,6 @@ def render_scene_handler(*args, **kwargs):
     :param kwargs:
     :return:
     """
-    global idx, cam
-
-    scene = bpy.context.scene
-    render = scene.render
 
     # Randomize head pose and color
     randomize_head_pose(head, hmd)
@@ -62,23 +78,25 @@ def render_scene_handler(*args, **kwargs):
         light.data.node_tree.nodes['Emission'].inputs['Color'].default_value = random_color(min=100., max=200.)
         light.data.node_tree.nodes['Emission'].inputs['Strength'].default_value = np.random.uniform(0., 0.1)
 
+
+def save_semseg_handler(*args, **kwargs):
+    dst_file = os.path.join(OUTPUT_DIR, 'gt', f'{idx}.png')
+    shutil.copy(GT_IMAGE, dst_file)
+    print(f'########## SAVED GROUND TRUTH FILE: {idx} ##############')
+
+
+bpy.app.handlers.render_complete.append(randomize_scene_handler)
+bpy.app.handlers.render_post.append(save_semseg_handler)
+
+NR_OF_RENDERS = 40000
+
+for idx in range(NR_OF_RENDERS):
+    scene = bpy.context.scene
+    render = scene.render
+
     # Render image
     print(f'########## RENDERING FILE: {idx} ##############')
     render.image_settings.file_format = 'PNG'
     render.filepath = os.path.join(OUTPUT_DIR, 'rgb', f'{idx}.png')
-    idx += 1
     bpy.ops.render.render(write_still=True)
-
-
-def save_semseg_handler(*args, **kwargs):
-    """ Save the previously rendered semantic segmentation image to correct file. """
-    global idx
-    # Copy ground truth image
-    dst_file = os.path.join(OUTPUT_DIR, 'gt', f'{idx - 1}.png')
-    shutil.copy(GT_IMAGE, dst_file)
-
-    print(f'########## SAVED GROUND TRUTH FILE: {idx - 1} ##############')
-
-bpy.app.handlers.render_complete.append(render_scene_handler)
-bpy.app.handlers.render_post.append(save_semseg_handler)
-render_scene_handler()
+    idx += 1